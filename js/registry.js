06d097da83d425ab3f6131d60a1781aa63788f30
youseop <youseop.lee@voyagerx.com>
2021-11-09 22:08:44 +0900

    add renderWrapper function description

--- js/registry.js
+++ js/registry.js
@@ -17,6 +17,16 @@ export const renderWrapper = (component) => {
   };
 };
 
+/* 
+1. renderRoot로 div.root가 반영된다.
+2. div.root내부의 app component에 appView로 노드를 형성하고 추가할 준비를 한다.
+3. 이 과정에서 appView에 의해 만들어진 노드 하위에 
+    counter, filter, todos componet가 존재하기 때문에
+    다시 재귀적으로 각컴포넌트에 ~~View로 노드를 형성하고 추가할 준비를 한다.
+4. appView로 형성된 노드 내에 각각의 컴포넌트를 추가하면 app component 가 완성된다.
+5. app component를 루트에 추가한다.
+*/
+
 export const add = (name, component) => {
   registry[name] = renderWrapper(component);
 };
