ef187de4b4a48e288ee7f60406b8270c22d3eb71
youseop <youseop.lee@voyagerx.com>
2021-11-09 21:58:59 +0900

    divide template

--- js/applyDiff.js
+++ js/applyDiff.js
@@ -1,40 +1,10 @@
-import getTodos from "./getTodos.js";
-import todosView from "./view/todos.js";
-import counterView from "./view/counter.js";
-import filtersView from "./view/filters.js";
-
-import * as registry from "./registry.js";
-import { all } from "./constant.js";
-
-registry.add("todos", todosView);
-registry.add("counter", counterView);
-registry.add("filters", filtersView);
-
-const state = {
-  todos: getTodos(),
-  currentFilter: all,
-};
-
-//모든 dom조작이나 애니메이션은 이 DOM API를 기반으로 해야 한다.
-/*이 API는 메인 스레드를 차단하지 않으며 
-repaint가 이벤트 루프에서 스케줄링 되기 직전에 실행된다.*/
-const render = () => {
-  requestAnimationFrame(() => {
-    const main = document.querySelector(".todoapp");
-    const newMain = registry.renderRoot(main, state);
-    applyDiff(document.body, main, newMain);
-  });
-};
-
-render();
-
 /*
 applyDiff function
 1. 새 노드가 정의되지 않은 경우 실제 노드를 삭제한다.
 2. 실제 노드가 정의되지 않았지만 가상노드가 존재하는 경우 부모 노드에 추가한다.
 3. 두 노드가 모두 정의된 경우 두 노드 간에 차이가 있는지 확인한다.
 */
-const applyDiff = (parentNode, realNode, virtualNode) => {
+export const applyDiff = (parentNode, realNode, virtualNode) => {
   if (realNode && !virtualNode) {
     realNode.remove();
     return;
---
+++ js/index.js
@@ -0,0 +1,32 @@
+import getTodos from "./getTodos.js";
+import appView from "./view/app.js";
+import todosView from "./view/todos.js";
+import counterView from "./view/counter.js";
+import filtersView from "./view/filters.js";
+import { applyDiff } from "./applyDiff.js";
+
+import * as registry from "./registry.js";
+import { all } from "./constant.js";
+
+registry.add("app", appView);
+registry.add("todos", todosView);
+registry.add("counter", counterView);
+registry.add("filters", filtersView);
+
+const state = {
+  todos: getTodos(),
+  currentFilter: all,
+};
+
+//모든 dom조작이나 애니메이션은 이 DOM API를 기반으로 해야 한다.
+/*이 API는 메인 스레드를 차단하지 않으며 
+repaint가 이벤트 루프에서 스케줄링 되기 직전에 실행된다.*/
+const render = () => {
+  requestAnimationFrame(() => {
+    const main = document.querySelector("#root");
+    const newMain = registry.renderRoot(main, state);
+    applyDiff(document.body, main, newMain);
+  });
+};
+
+render();
